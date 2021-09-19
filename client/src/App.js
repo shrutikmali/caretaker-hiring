226eeea9b2f61e2a74c1dc3f10ac9bbf79c1e22e
Shrutik <shrutik.mali@somaiya.edu>
2021-09-19 14:56:43 +0530

    Add react router, login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
-  return <h1>Hello World</h1>
+  return (
+    <Router>
+      <Switch>
+        <Route path='/login' exact>
+          <Login />
+        </Route>
+        <Route path='/register' exact>
+          <Register />
+        </Route>
+        <Route path = '/' exact>
+          <p>Go to localhost:3000/login for login</p>
+          <p>Go to localhost:3000/register for register</p>
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
