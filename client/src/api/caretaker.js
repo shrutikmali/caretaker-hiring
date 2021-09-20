c0ee34133d444e56ab5f014a7439f521acb7da68
Shrutik <shrutik.mali@somaiya.edu>
2021-09-20 15:06:31 +0530

    Create sign in routes

--- client/src/api/caretaker.js
+++ client/src/api/caretaker.js
@@ -1,2 +1,6 @@
 import axios from 'axios';
 
+const URL = 'http://localhost:5000/customer';
+
+export const signIn = (credentials) => axios.post(`${URL}/signin`, credentials);
+
--- client/src/api/customer.js
+++ client/src/api/customer.js
@@ -1,2 +1,5 @@
 import axios from 'axios';
 
+const URL = 'http://localhost:5000/customer';
+
+export const signIn = (credentials) => axios.post(`${URL}/signin`, credentials);
